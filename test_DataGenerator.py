5b6683747fcbdd20909fa42cffb154353c5262bf
Karthikeyani Shanmuga Sundaram (BGSW/ETS) <ShanmugaSundaram.Karthikeyani@in.bosch.com>
2023-02-17 12:31:11 +0530

    modified test code

--- test_DataGenerator.py
+++ test_DataGenerator.py
@@ -6,7 +6,7 @@ from DataGenerator import *
 class generateTester(unittest.TestCase):
     def test(self):  
         gen = DataGenerator()
-        self.assertTrue(gen.generateBatteryData()['SOC'] in range(gen.socMin,gen.socMax+1))
-        self.assertTrue(gen.generateBatteryData()['T'] in range(gen.tMin,gen.tMax+1))
+        output = gen.generateBatteryData()
+        self.assertTrue(output['SOC'] in range(gen.socMin,gen.socMax+1) and output['T'] in range(gen.tMin,gen.tMax+1))
 if __name__ == '__main__':
     unittest.main()
