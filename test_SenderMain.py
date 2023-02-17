06fcbd065f595ea1fabd8a6a4a0c05ca662744c3
Karthikeyani Shanmuga Sundaram (BGSW/ETS) <ShanmugaSundaram.Karthikeyani@in.bosch.com>
2023-02-17 13:01:52 +0530

    bug fix

--- test_SenderMain.py
+++ test_SenderMain.py
@@ -5,8 +5,8 @@ from SenderMain import *
 class SenderMainTester(unittest.TestCase):
     def test(self):  
         stub = StubCode()
-        sender = Sender()
-        sender.sendData(generatorFunction = stub.stubDataGenerator() ,formatterFunction = stub.stubOutputFormatter ,printerFunction = stub.stubPrintToConsole)
+        sender = Sender(generatorFunction = stub.stubDataGenerator() ,formatterFunction = stub.stubOutputFormatter ,printerFunction = stub.stubPrintToConsole)
+        sender.sendData()
         self.assertTrue(stub.callCountStubDataGenerator == 1)
         self.assertTrue(stub.callCountStubOutputFormatter == 1)
         self.assertTrue(stub.callCountStubDataGenerator == 1)
